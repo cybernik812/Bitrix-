605a6ac975d3ea2c0f2fd0c376894a8ef4337111
SkillUP Student <student@skillup.spb.ru>
2018-01-17 22:11:05 +0300

    The second commit

--- index.php
+++ index.php
@@ -60,5 +60,7 @@ $APPLICATION->SetTitle("Статьи");
         "AJAX_OPTION_HISTORY" => "N",
         "AJAX_OPTION_ADDITIONAL" => ""
     )
-);
+);?>
+
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
